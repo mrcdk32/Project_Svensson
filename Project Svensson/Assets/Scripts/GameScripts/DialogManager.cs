a91e0eaa0b4c2fa804dd577e077cf5c7ca5136ff
mrcdk32 <55589410+mrcdk32@users.noreply.github.com>
2019-10-21 22:58:43 +0200

    Update pre_alpha 0.1
    *Added a dialog system, with a clue sprite to help the player know where he/she can enter a dialog.
    *Updated Enemy AI, it can now follow, and flee if the player is to far away or to close. The new AI can also patrol a area depending on what coordinates you set.
    *Added scene transitions, with scene transition animations(WOP)
    *General fixes and improvments

---
+++ Project Svensson/Assets/Scripts/GameScripts/DialogManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogText;
+    public Animator anim;
+    private Queue<string> sentence;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentence = new Queue<string>();
+    }
+
+
+    public void StartDialog(Dialog dialog)
+    {
+        anim.SetBool("isOpen", true);
+        nameText.text = dialog.name;
+        sentence.Clear();
+
+        foreach(string sentences in dialog.sentence)
+        {
+            sentence.Enqueue(sentences);
+        }
+        DisplayNextSentence();
+
+    }
+    public void DisplayNextSentence()
+    {
+      
+            if (sentence.Count == 0)
+            {
+               EndDialog();
+                return;
+            }
+            string sentences = sentence.Dequeue();
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentences));
+ 
+    }
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+   void EndDialog()
+    {
+        anim.SetBool("isOpen", false);
+    }
+   
+}
---
+++ Project Svensson/Assets/Scripts/GameScripts/DialogTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialog dialog;
+    public bool playerInRange;
+    public Signal_Event context;
+
+    public virtual void Update()
+    {
+        if (Input.GetButtonDown("Attack") && playerInRange)
+        {
+            FindObjectOfType<DialogManager>().StartDialog(dialog);
+
+
+        }
+        else if (Input.GetButtonDown("Next"))
+        {
+            FindObjectOfType<DialogManager>().DisplayNextSentence();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            context.Raise();
+            playerInRange = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
+    }
+}
